28090dc6d52b46b93440e0b783ad0f3c858fa7d7
tonybaas <tonyrrbaas@gmail.com>
2024-03-08 21:59:29 -0600

    Changes: -Updated Nav bar

--- Leavewithasmilecleaningwebsite/Controllers/HomeController.cs
+++ Leavewithasmilecleaningwebsite/Controllers/HomeController.cs
@@ -1,24 +1,16 @@
-﻿//using Leavewithasmilecleaningwebsite.Models;
+﻿
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-
 namespace Leavewithasmilecleaningwebsite.Controllers
 {
     public class HomeController : Controller
     {
-        //private LeavewithasmilecleaningwebsiteContext context { get; set; }
-
-        //public HomeController(LeavewithasmilecleaningwebsiteContext ctx)
-        //{
-        //    context = ctx;
-        //}
 
         public IActionResult Index()
         {
             return View();
         }
 
-        [Route("about")]
         public IActionResult About()
         {
             return View();
