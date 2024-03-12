e1521273afde3a28d0b2e297e8cef65d73e58d30
Nicholas Woerner <nicholaswoerner@gmail.com>
2024-03-11 22:33:40 -0500

    Added Residential page and routed nav towards it.

--- Leavewithasmilecleaningwebsite/Controllers/HomeController.cs
+++ Leavewithasmilecleaningwebsite/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace Leavewithasmilecleaningwebsite.Controllers
         {
             return View();
         }
+
+        public IActionResult Residential()
+        {
+            return View();
+        }
     }
 }
